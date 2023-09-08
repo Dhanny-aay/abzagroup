ed183d9236adf7557f704ea6c8f2ab364f95942f
Dhanny-aay <danielakintolu@gmail.com>
2023-09-08 21:50:50 +0100

    added swipe animation

--- src/comps/hero.js
+++ src/comps/hero.js
@@ -1,12 +1,115 @@
 import angleright from '../images/Angle-right.svg';
 import mobright from '../images/mobright.svg';
 import swipe from '../images/swipe.svg';
+import ceo from '../images/ceo.svg';
+import bizHero from '../images/bizhero.svg';
+import heroThree from '../images/heroThree.svg';
+import heroFour from '../images/heroFour.svg';
+import mobHero from '../images/mobHero.svg';
+import bizheromob from '../images/bizheromob.svg'
+import { useState, useEffect } from 'react';
+import { motion, useAnimation } from 'framer-motion';
  
 const Hero = () => {
+    // swipe function
+    const [startX, setStartX] = useState(null);
+    const [endX, setEndX] = useState(null);
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const controls = useAnimation();
+
+    const desktopBackgroundImageUrls = [
+        ceo,
+        bizHero,
+        heroThree,
+        heroFour
+    ];
+
+    const mobileBackgroundImageUrls = [
+        mobHero,
+        bizheromob,
+    ];
+
+    const isMobileScreen = window.innerWidth <= 768;
+
+    useEffect(() => {
+        const handleResize = () => {
+          // Check if the screen size has changed and update isMobileScreen
+          const newIsMobileScreen = window.innerWidth <= 768; // Adjust the breakpoint as needed
+          if (newIsMobileScreen !== isMobileScreen) {
+            setCurrentIndex(0); // Reset the index when switching between mobile and desktop
+          }
+        };
+    
+        window.addEventListener('resize', handleResize);
+    
+        return () => {
+          window.removeEventListener('resize', handleResize);
+        };
+    }, [isMobileScreen]);
+
+    const backgroundImageUrls = isMobileScreen
+    ? mobileBackgroundImageUrls
+    : desktopBackgroundImageUrls;
+
+    const Herotxt = [
+        'A speech from our CEO',
+        'Transformation is here',
+        'Transformation is here',
+        'Transformation is here',
+    ];
+
+    const handleTouchStart = (e) => {
+        setStartX(e.touches[0].clientX);
+      };
+    
+      const handleTouchMove = (e) => {
+        setEndX(e.touches[0].clientX);
+      };
+    
+      const handleTouchEnd = () => {
+        if (startX !== null && endX !== null) {
+          const swipeDistance = endX - startX;
+    
+          if (swipeDistance > 50) {
+            // Swipe right
+            controls.start({ x:'-5%' }); // Animate to the left
+            setCurrentIndex((prevIndex) =>
+            prevIndex === 0 ? backgroundImageUrls.length - 1 : prevIndex - 1
+        );
+          } else if (swipeDistance < -50) {
+            // Swipe left
+            controls.start({ x:'5%' }); // Animate to the right
+            setCurrentIndex((prevIndex) =>
+            prevIndex === backgroundImageUrls.length - 1 ? 0 : prevIndex + 1
+            );
+          }
+        }
+
+        // Reset the start and end values after a delay
+        setTimeout(() => {
+          setStartX(null);
+          setEndX(null);
+          controls.start({ x: '0%' }); // Reset position to initial
+        }, 300); // Adjust the delay as needed to match the animation duration
+      };
+    
+      const currentBackgroundImageUrl = backgroundImageUrls[currentIndex];
+;
+
     return ( 
         <>
-        <div className=" md:p-14 px-3  py-5 w-full h-[90vh]">
-            <div className=" w-full h-[100%] bg-yellow-100 rounded-[30px] ceoHero bg-cover relative">
+        <div className=" md:p-14 px-3  py-5 w-full h-[90vh] swipe-container-parent">
+            <motion.div
+            onTouchStart={handleTouchStart}
+            onTouchMove={handleTouchMove}
+            onTouchEnd={handleTouchEnd}
+            style={{
+                background: `url(${currentBackgroundImageUrl})`,
+            }}
+            animate={controls}
+            transition={{stiffness:0}}
+            className=" w-full h-[100%] swipe-container bg-yellow-100 rounded-[30px] bg-cover relative">
                 <span className=" absolute bottom-8 left-4 md:bottom-[64px] md:left-[64px] space-y-4">
                     <p className=" font-Mulish font-semibold text-[24px] md:text-[48px] text-[#fff]">A speech from our CEO</p>
                     <span className=" flex flex-row space-x-2 items-center">
@@ -18,7 +121,7 @@ const Hero = () => {
                 <span className=' absolute bottom-5 w-full hidden md:flex justify-center items-center'>
                     <img src={ swipe } className='' alt="" />
                 </span>
-            </div>
+            </motion.div>
         </div>
         </>
      );
